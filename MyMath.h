c7af3e6addbf4b06433335b4f61d76f0cf273c20
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-04-15 12:42:55 +0900

    完了

--- MyMath.h
+++ MyMath.h
@@ -54,3 +54,8 @@ Matrix4x4 Inverse(const Matrix4x4& m);
 Matrix4x4 Transpose(const Matrix4x4& m);
 Matrix4x4 MakeIdentity4x4();
 void MatrixScreenPrintf(int x, int y, Matrix4x4& matrix, const char* label);
+
+// 00_03
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
--- main.cpp
+++ main.cpp
@@ -13,19 +13,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Matrix4x4 m1 = { 
-	3.2f,0.7f,9.6f,4.4f,
-	5.5f,1.3f,7.8f,2.1f,
-	6.9f,8.0f,2.6f,1.0f,
-	0.5f,7.2f,5.1f,3.3f};
-	
-	Matrix4x4 m2 = {
-	4.1f,6.5f,3.3f,2.2f,
-	8.8f,0.6f,9.9f,7.7f,
-	1.1f,5.5f,6.6f,0.0f,
-	3.3f,9.9f,8.8f,2.2f
-	};
-
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -39,14 +26,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 		
-		Matrix4x4 resultAdd = Add(m1, m2);
-		Matrix4x4 resultSub = Subtract(m1, m2);
-		Matrix4x4 resultMul = Multiply(m1, m2);
-		Matrix4x4 resultInverseM1 = Inverse(m1);
-		Matrix4x4 resultInverseM2 = Inverse(m2);
-		Matrix4x4 resultTransposeM1 = Transpose(m1);
-		Matrix4x4 resultTransposeM2 = Transpose(m2);
-		Matrix4x4 identity = MakeIdentity4x4();
+		Vector3 translate{ 4.1f,2.6f,0.8f };
+		Vector3 scale{ 1.5f,5.2f,7.3f };
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Vector3 point{ 2.3f,3.8f,1.4f };
+		Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f
+		};
+
+		Vector3 transformed = Transform(point, transformMatrix);
 
 		///
 		/// ↑更新処理ここまで
@@ -56,15 +48,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0,0,resultAdd, "Add");
-		MatrixScreenPrintf(0, 100, resultSub, "Subtract");
-		MatrixScreenPrintf(0, 200, resultMul, "Multiply");
-		MatrixScreenPrintf(0, 300, resultInverseM1, "InverseM1");
-		MatrixScreenPrintf(0, 400, resultInverseM2, "InverseM2");
-		MatrixScreenPrintf(400, 0, resultTransposeM1, "TransposeM1");
-		MatrixScreenPrintf(400, 100, resultTransposeM2, "TransposeM2");
-		MatrixScreenPrintf(400, 200, identity, "Identity");
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, 20, translateMatrix, "transformMatrix");
+		MatrixScreenPrintf(0, 120, scaleMatrix, "scaleMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
